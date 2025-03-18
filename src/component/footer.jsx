dfb2647ecaa4b32bb3774f8a12fd0fb951c8d9ba
Huỳnh Thanh Phương <119808395+phuong261104@users.noreply.github.com>
2025-03-18 08:40:59 +0700

    hoàn thành tuần 5

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -3,28 +3,28 @@ import "./footer.css"; // Import file CSS
 
 const Footer = () => {
     return (
-        <footer className="footer">
-            <div className="footer-section about">
-                <h3>About Us</h3>
-                <p>
+        <footer className="bg-black text-white p-8 flex flex-wrap justify-center gap-7">
+            <div className="mb-5 min-w-[200px] about">
+                <h3 className="text-[18px] mb-2.5">About Us</h3>
+                <p className="text-[14px] leading-[1.5]">
                     Welcome to our website, a wonderful place to explore and learn how to
                     cook like a pro.
                 </p>
-                <div className="subscribe">
-                    <input type="email" placeholder="Enter your email" />
-                    <button>Send</button>
+                <div className="flex mt-2">
+                    <input className="flex-1 bg-white mr-2 text-black p-2 rounded-md outline-none focus:ring-0" type="email" placeholder="Enter your email" />
+                    <button className="bg-pink-500 text-white px-3 rounded-md cursor-pointer transition duration-300 hover:bg-pink-300">Send</button>
                 </div>
-                <div className="footer-bottom">
+                <div className="flex w-full text-center pt-2 mt-5 text-xs ">
                     <div className="footer-logo">
-                        <span className="logo">Chefify</span>
+                        <span className="text-lg font-bold">Chefify</span>
                     </div>
-                    <p> <span className="logo">Chefify</span>© 2023 Chefify Company | Terms of Service | Privacy Policy</p>
+                    <p> <span className="text-lg font-bold">Chefify</span>© 2023 Chefify Company | Terms of Service | Privacy Policy</p>
                 </div>
             </div>
 
-            <div style={{ display: "flex", flexDirection: "column", justifyContent: "space-between" }}>
-                <div className="footer-section">
-                    <h3>Learn More</h3>
+            < div className="flex flex-col justify-between">
+                <div className="min-w-[200px] mb-5">
+                    <h3 className="text-base mb-2">Learn More</h3>
                     <ul>
                         <li>Our Cooks</li>
                         <li>See Our Features</li>
@@ -32,8 +32,8 @@ const Footer = () => {
                     </ul>
                 </div>
 
-                <div className="footer-section">
-                    <h3>Shop</h3>
+                <div className="min-w-[200px] mb-5">
+                    <h3 className="text-base mb-2">Shop</h3>
                     <ul>
                         <li>Gift Subscription</li>
                         <li>Send Us Feedback</li>
@@ -41,8 +41,8 @@ const Footer = () => {
                 </div>
             </div>
 
-            <div className="footer-section">
-                <h3>Recipes</h3>
+            <div className="min-w-[200px] mb-5">
+                <h3 className="text-base mb-2">Recipes</h3>
                 <ul>
                     <li>What to Cook This Week</li>
                     <li>Pasta</li>
--- src/component/item.jsx
+++ src/component/item.jsx
@@ -1,4 +1,4 @@
-import "./item.css";
+import "./item.css";//memo, useMemo, useCallback, useReducer
 
 const Item = (props) => {
     const { dataCard } = props;
@@ -6,19 +6,19 @@ const Item = (props) => {
     return (
         <>
             {dataCard && dataCard.map((item) => (
-                <div className="card" key={item.id}>
-                    <div className="card-img">
+                <div className="bg-white w-[300px] rounded-lg border border-gray-300 shadow-md" key={item.id}>
+                    <div className="w-full h-auto block">
                         <img
                             src={item.avatar}
                             alt={item.title}
                         />
                     </div>
-                    <div className="card-body">
-                        <div className="card-body__header">
-                            <h3 className="card-body__title">{item.title}</h3>
-                            <span className="card-body__bookmark">🔖</span>
+                    <div className="p-[12px 16px]">
+                        <div className="flex items-center justify-between mb-2">
+                            <h3 className="text-lg text-gray-800 font-semibold">{item.title}</h3>
+                            <span className="text-lg cursor-pointer hover:opacity-80">🔖</span>
                         </div>
-                        <div className="card-body__time">{item.time}</div>
+                        <div className="text-md text-gray-500 p-[10px]">{item.time}</div>
                     </div>
                 </div>
             ))}
